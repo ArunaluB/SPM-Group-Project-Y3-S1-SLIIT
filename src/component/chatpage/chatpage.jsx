a3a1f1f6b2cd85f07c2a68c85666745f40ddb14f
ArunaluB <IT22515612@my.sliit.lk>
2024-09-12 18:14:37 +0530

    routers add and insies se

---
+++ src/component/chatpage/chatpage.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './chatpage.css'
+const chatpage = () => {
+  return (
+    <div>chatpage</div>
+  )
+}
+
+export default chatpage
---
+++ src/component/dashboard/dashboard.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './dashboard.css'
+const dashboard = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default dashboard
