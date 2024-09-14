e2edbcf95e4bc3b7149e82b84d8d026ee2b9cb0c
ArunaluB <IT22515612@my.sliit.lk>
2024-09-14 17:45:48 +0530

    dashboad css

--- src/component/dashboard/dashboard.jsx
+++ src/component/dashboard/dashboard.jsx
@@ -1,9 +1,43 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './dashboard.css'
+import logo from '../../assets/logo.png'
+import img1 from '../../assets/chat.png'
+import img2 from '../../assets/image.png'
+import img3 from '../../assets/code.png'
+import buttonimg from '../../assets/arrow.png'
 const dashboard = () => {
   return (
-    <div>dashboard</div>
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <h1>PRO MENTER</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src={img1} alt="option 1" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src={img2} alt="option 1" />
+            <span>Upload Image</span>
+          </div>
+          <div className="option">
+            <img src={img3} alt="option 1" />
+            <span>Help me with my Code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form action="">
+          <input type="text" placeholder='Ask me Eroor or Help' />
+          <button>
+            <img src={buttonimg} alt="submit" />
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/layouts/rootlayout/RootLayout.jsx
+++ src/layouts/rootlayout/RootLayout.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import './rootlayout.css'
 import { Link, Outlet } from 'react-router-dom'
-import logo from './logo.png'
+import logo from '../../assets/logo.png'
 
 const RootLayout = () => {
     return (
