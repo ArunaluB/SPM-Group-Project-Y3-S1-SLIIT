41176866cb653c4b97767841ef4d9b8a88bb7025
ArunaluB <IT22515612@my.sliit.lk>
2024-09-14 16:19:43 +0530

    chatside

--- src/layouts/dashboardLayout/dashboardLayout.jsx
+++ src/layouts/dashboardLayout/dashboardLayout.jsx
@@ -2,10 +2,11 @@
 import React from 'react'
 import './dashboardLayout.css'
 import { Outlet } from 'react-router-dom'
+import ChartList from '../../component/chartList/ChartList'
 const dashboardLayout = () => {
   return (
     <div className="dashboardLayout">
-        <div className="menu">MENU</div>
+        <div className="menu"><ChartList/></div>
         <div className="content">
             <Outlet />
         </div>
